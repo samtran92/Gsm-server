36dcd7b3f841e76906b08da7c24440e3284a2b03
doancongthang <55833222+doancongthang@users.noreply.github.com>
2021-11-17 10:38:39 +0700

    ok main

--- 18_Jun_2021/Controllers/AccountController.cs
+++ 18_Jun_2021/Controllers/AccountController.cs
@@ -191,7 +191,7 @@ namespace _18_Jun_2021.Controllers
         static GMSDevice_type GMSDevice;
         SelectListItem drPorts = new SelectListItem()
         {
-            Text = "COM11",
+            Text = "COM7",
             Value = "1",
             Selected = true
         };
